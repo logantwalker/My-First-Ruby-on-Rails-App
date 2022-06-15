da72a003fd93aa2670e5c0f818418ec07737b8b7
L.Walker <logantwalker95@gmail.com>
2022-06-15 15:15:39 -0400

    starting on POST functionality

--- frontend/src/features/Posts/PostForm.tsx
+++ frontend/src/features/Posts/PostForm.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
 
 function PostForm(){
+    const dispatch: AppDispatch = useDispatch()
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const submitHandler = (e) =>{
+        e.preventDefault();
+
+        const formData = {
+            post : {
+                title: title,
+                body: body
+            }
+        }
+
+        dispatch(createPostAsync(formData));
+        resetState();
+    }
+
+    const resetState = () =>{
+        setTitle('');
+        setBody('');
+    }
+
     return(
-        <div>form</div>
+        <React.Fragment>
+            <h1>Post Form</h1>
+            <form>
+                <input type='text' className="form-control text-start" name='title' value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <textarea className="form-control text-start" name='body' value={body} onChange={e => setBody(e.target.value)}/>
+                <button type='submit' onClick={e => submitHandler(e)}>Submit</button>
+            </form>
+        </React.Fragment>
     )
 }
 
